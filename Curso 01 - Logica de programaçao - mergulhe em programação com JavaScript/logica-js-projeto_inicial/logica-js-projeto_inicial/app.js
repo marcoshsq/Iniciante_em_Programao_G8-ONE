82a0b8ee8d2853c020d6cfd0ee76d082332843c4
Marcos <marcoshsq@gmail.com>
2025-01-17 22:45:34 -0300

    Exercicios_resolvidos

---
+++ Curso 01 - Logica de programaçao - mergulhe em programação com JavaScript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert("Bem vindo ao jogo de adivinhação");
+let numero_secreto = 10;
+let numero_usuario = prompt("Escolha um número entre 1 e 10:");
+if(numero_secreto == numero_usuario) {
+    console.log("Acertou, miserávi")
+}
+
